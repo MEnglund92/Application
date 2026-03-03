81c26035676696d26f8b299c62c01b9b4d2a7432
Zokie92 <mattias.englund1992@gmail.com>
2026-03-03 13:25:02 +0100

    .

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -15,5 +15,10 @@ export default defineConfig({
         alias: {
             '@': '/src'
         }
+    },
+    esbuild: {
+        loader: "jsx",
+        include: /src\/.*\.[tj]sx?$/,
+        exclude: []
     }
 })
