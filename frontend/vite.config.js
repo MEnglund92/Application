007694d087b8c1d69ddb6939e66ca8b5b9a64aa0
Zokie92 <mattias.englund1992@gmail.com>
2026-03-03 13:20:10 +0100

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,9 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var vite_1 = require("vite");
-var plugin_react_1 = require("@vitejs/plugin-react");
-exports.default = (0, vite_1.defineConfig)({
-    plugins: [(0, plugin_react_1.default)()],
+cat > vite.config.js << 'EOF'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+    plugins: [react()],
     server: {
         port: 3000,
         host: true
@@ -17,4 +17,5 @@ exports.default = (0, vite_1.defineConfig)({
             '@': '/src'
         }
     }
-});
+})
+EOF
