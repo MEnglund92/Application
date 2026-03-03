2e614011e734240441752fc0f94837b83ccc1053
Zokie92 <mattias.englund1992@gmail.com>
2026-03-03 10:51:29 +0100

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -10,5 +10,10 @@ export default defineConfig({
     build: {
         outDir: 'dist',
         sourcemap: true
+    },
+    resolve: {
+        alias: {
+            '@': '/src'
+        }
     }
 })
